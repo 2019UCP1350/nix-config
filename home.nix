aed8c9b63f288ce9fb7da703134e0cc95302cd04
manvir singh <manvir.singh@manvir.singh-MacBookPro>
2023-04-25 22:20:44 +0530

    added nix config changes

--- home.nix
+++ home.nix
@@ -13,6 +13,7 @@
   home.packages = with pkgs; [
     nix-output-monitor # https://github.com/maralorn/nix-output-monitor
     nix-info
+    kitty
   ];
 
   # Programs natively supported by home-manager.
